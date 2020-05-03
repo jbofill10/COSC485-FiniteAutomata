b7baf30440f737c217339f2f8e5f489a3a32f486
jbofill10 <juan.bofill10@gmail.com>
2020-05-03 19:47:45 -0400

    Add DFA algorithm that checks whether a string is acceptable to the machine or not

--- src/components/DFA/DFA.jsx
+++ src/components/DFA/DFA.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Network, Node, Edge } from 'react-vis-network';
+import fs from "fs";
+import 'dotenv/config'
 
 
 export default class DFA extends React.Component{
@@ -19,6 +21,10 @@ export default class DFA extends React.Component{
 
     componentWillUnmount() {
         window.removeEventListener("resize", this.updateWindowDimensions.bind(this));
+    }
+
+    componentDidUpdate(){
+        this.computeStringsOnDFA();
     }
 
     updateWindowDimensions() {
@@ -52,9 +58,9 @@ export default class DFA extends React.Component{
                     {/* Building States for Graph */}
                     {this.props.states.map((state, index) => {
                         if (this.props.startingState === state){
-                            return <Node key={index} id={state} label={state} color={'#ffb6b6'}/>
+                            return <Node key={index} id={state} label={state} color={'#75daad'}/>
                         }else if(this.props.finalStates.includes(state)){
-                            return <Node key={index} id={state} label={state} color={'#900c3f'}/>
+                            return <Node key={index} id={state} label={state} color={'#f34573'}/>
                         }else{
                             return <Node key={index} id={state} label={state}/>
                         }
@@ -77,6 +83,65 @@ export default class DFA extends React.Component{
                 <div>Loading...</div>
             )
         }
+    }
+
+    computeStringsOnDFA = () => {
+        var strings = fs.readFileSync(String(process.env.REACT_APP_file2)).toString("utf-8").split("\n").filter(e => e.length > 1);
+
+        // String hashmap to contain whether its true or false
+        var stringAcceptance = {};
+
+        // Set all strings in the hashmap with a value that is false
+        // If the string is accepted in the algorithm, it is set to true.
+        strings.forEach(string => {
+            stringAcceptance[string] = false;
+        })
+        
+        var currentState = "";
+
+        strings.forEach(string => {
+            
+            // Used to track how far a string has progressed. At the end,
+            // if the string is acceptable, the length should be 1. (Since for loop wouldn't continue)
+            var stringLength = string.length-1;
+            
+            // Considering String could be extremely long, I'd rather access from
+            // an array vs. using charAt
+            var stringArray = string.trim().split("");
+            currentState  = this.props.startingState;
+
+            for (var i = 0; i < stringArray.length; i++){
+                var currentChar = stringArray[i];
+                // Does current state contain a transition function? If not, next character
+                if (this.props.transitionFunctions.hasOwnProperty(currentState)){
+                    this.props.transitionFunctions[currentState].forEach(transition => {
+                        
+                        // Technically object.entries returns a 2D array, but it will always be one key value pairing
+                        // So I can always use [0][0] and [0][1] to get the values I want 100% of the time 
+                        var transitionState = Object.entries(transition)[0][0];
+                        var transitionChar = Object.entries(transition)[0][1];
+
+                        // If transition function uses current character
+                        if (transitionChar === currentChar){
+                            currentState = transitionState;
+                            stringLength--;
+                            // If all characters have been processed and is in a final state
+                            if (stringLength == 0 && this.props.finalStates.includes(currentState)){
+                                stringAcceptance[string] = true;
+                            }
+                            return;
+                        }  
+                    })
+                }else{
+                    // JavaScript's .forEach function does not let you use continue/break
+                    // Return has the same effect as continue (moving to next char).
+                    return;
+                }
+            }
+        })
+
+        console.log(stringAcceptance);
+
     }
 
 
